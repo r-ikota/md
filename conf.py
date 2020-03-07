cef97018cd3bcdb9b903341896089d516a980dfe
Ryo Ikota <r.ikota.mt@gmail.com>
2020-03-07 17:18:50 +0900

    I gave up configuring readthedocs.

--- conf.py
+++ conf.py
@@ -30,7 +30,7 @@ author = 'Ryo Ikota'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 # extensions = ['recommonmark']
-extensions = [ 'sphinxcontrib.spelling' ]
+# extensions = [ 'sphinxcontrib.spelling' ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
